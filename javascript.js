53385d4a7417c02f5c3fec59982cad0536f15bc4
hbfrm <86564794+hbfrm@users.noreply.github.com>
2023-09-25 00:37:23 +0200

    Add some pseudocode and create first function.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-console.log('Hello world');
+// Get computer's choice
+function getComputerChoice() {
+
+  return choice;
+}
+// Assign it to a variable 
+// Get player's choice
+  // make it case-insensitive
+// Assign it to a variable
+//
+// Check who won
+// Output the result
