b5a0de2fd0adeb071cb57871194c1b0e15200bd3
hbfrm <86564794+hbfrm@users.noreply.github.com>
2023-09-25 00:52:43 +0200

    Define chocies, add random int function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,15 @@
+// Get random number from 0 to 2
+function getRandomInt() {
+  return Math.floor(Math.random() * 3);
+}
+
+// Define rock paper scissors const
+const rock = "Rock";
+const paper = "Paper";
+const scissors = "Scissors";
+
+// Assign choice to number
+function assignChoiceToInt()
 // Get computer's choice
 function getComputerChoice() {
 
