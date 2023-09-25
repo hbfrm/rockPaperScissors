1ba8075f499ca5c7a468fd2b8f0c6964ce955d3d
hbfrm <86564794+hbfrm@users.noreply.github.com>
2023-09-25 16:05:57 +0200

    Remove const.

--- javascript.js
+++ javascript.js
@@ -3,11 +3,6 @@ function getRandomInt() {
   return Math.floor(Math.random() * 3);
 }
 
-// Define rock paper scissors const
-const rock = "Rock";
-const paper = "Paper";
-const scissors = "Scissors";
-
 // Assign choice to number
 function assignChoiceToInt()
 // Get computer's choice
